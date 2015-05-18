28ff8c5ff9bcfd3d1b60b4ff33a2f371e3428621
Nate Rosenbloom <nrosenbloom123@gmail.com>
2015-05-18 11:19:37 -0400

    initial commit (with android build)

---
+++ build_android.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+NDK=$HOME/Downloads/android-ndk-r10e
+SYSROOT=$NDK/platforms/android-19/arch-arm/ 
+TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/
+function build_one
+{
+./configure \
+    --prefix=$PREFIX \
+    --disable-shared \
+    --enable-static \
+    --disable-doc \
+    --disable-ffmpeg \
+    --disable-ffplay \
+    --disable-ffprobe \
+    --disable-ffserver \
+    --disable-avdevice \
+    --disable-doc \
+    --disable-symver \
+    --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+    --target-os=linux \
+    --arch=arm \
+    --enable-cross-compile \
+    --sysroot=$SYSROOT \
+    --extra-cflags="-Os -fpic $ADDI_CFLAGS" \
+    --extra-ldflags="$ADDI_LDFLAGS" \
+    $ADDITIONAL_CONFIGURE_FLAG
+make clean
+make
+make install
+}
+CPU=arm
+PREFIX=$(pwd)/android/$CPU 
+ADDI_CFLAGS="-marm"
+build_one
